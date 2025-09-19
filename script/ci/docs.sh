9b37a1d7b791a3a0a849d216d295bb0bff0cc285
Maksymilian Goryszewski <mgoryszewski@internships.antmicro.com>
2025-09-19 11:42:16 +0200

    [#83752] Fixed docs deploy

--- script/ci/docs.sh
+++ script/ci/docs.sh
@@ -7,13 +7,12 @@ if [[ $(id -u) -ne 0 ]]; then
     SUDO='sudo'
 fi
 
-$SUDO apt -qqy install python3-venv python3-pip python3 texlive texlive-latex-recommended texlive-latex-extra texlive-fonts-extra latexmk > /dev/null
+$SUDO apt -qqy install python3-venv python3-pip python3 > /dev/null
 python3 -m venv .venv/
 source .venv/bin/activate
 
 pushd docs
   pip3 install -q -r requirements.txt
-  make html latexpdf
-  cp build/latex/*.pdf build/html/
+  make html
   tar cf docs.tar -C build/html/ .
 popd
