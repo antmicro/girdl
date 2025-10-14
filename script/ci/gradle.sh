8ad3e43a2f3e56124d3ce88174247381f9fc2f60
Maksymilian Goryszewski <mgoryszewski@internships.antmicro.com>
2025-10-14 10:39:37 +0200

    [#85263] Fix java PATH

--- script/ci/gradle.sh
+++ script/ci/gradle.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 export JAVA_HOME=$JAVA_HOME_21_X64
-export PATH=$JAVA_HOME:$PATH
-
 ./gradlew $@
--- script/ci/standalone.sh
+++ script/ci/standalone.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export JAVA_HOME=$JAVA_HOME_21_X64
+export PATH="$JAVA_HOME/bin":$PATH
+
 set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export DATA="$(pwd)/src/test/resources"
